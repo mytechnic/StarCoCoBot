b429185a7385c2d435e0c2db4d2b6f15bb6be2e0
mytechnic <mytechnic@gmail.com>
2021-04-07 23:58:53 +0900

    프레임워크 기본 구성

---
+++ src/main/java/mytechnic/starcoco/StarcocoApplication.java
@@ -0,0 +1,13 @@
+package mytechnic.starcoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarcocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarcocoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mytechnic/starcoco/StarcocoApplicationTests.java
@@ -0,0 +1,13 @@
+package mytechnic.starcoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarcocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
